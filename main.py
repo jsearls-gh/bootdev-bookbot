77c445da7765587172387e9ab61ca905823d100a
jsearls-gh <jsearls90@gmail.com>
2024-11-18 16:55:22 -0500

    refactor main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-def print_contents(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+def get_contents(file_path):
+    with open(file_path) as f:
+        return f.read()
+    
 
-def print_word_count(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+def print_contents(file_contents):
+   print(file_contents)
 
-def print_character_counts(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        lowered_contents = file_contents.lower()
-        char_counts = {}
-        for char in lowered_contents:
-            if char_counts.get(char) is None:
-                char_counts[char] = 1
-            else:
-                char_counts[char] += 1
+def print_word_count(file_contents):
+    words = file_contents.split()
+    print(len(words))
 
-        print(char_counts)
+def print_character_counts(file_contents):
+    lowered_contents = file_contents.lower()
+    char_counts = {}
+    for char in lowered_contents:
+        if char_counts.get(char) is None:
+            char_counts[char] = 1
+        else:
+            char_counts[char] += 1
+
+    print(char_counts)
             
 
 def main(path_to_file):
-    #print_contents(path_to_file)
-    #print_word_count(path_to_file)
-    print_character_counts(path_to_file)
+    file_contents = get_contents(path_to_file)
+    #print_contents(file_contents)
+    #print_word_count(file_contents)
+    print_character_counts(file_contents)
 
-main("books/frankenstein.txt")
+if __name__ == '__main__':
+    main("books/frankenstein.txt")
