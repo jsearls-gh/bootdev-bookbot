f6e4d515850159b0500919cbdc48328307b365ad
jsearls-gh <jsearls90@gmail.com>
2024-11-18 16:32:51 -0500

    update main.py to print word count

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main(path_to_file):
+def print_contents(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         print(file_contents)
+
+def print_word_count(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        print(len(words))
+
+def main(path_to_file):
+    #print_contents(path_to_file)
+    print_word_count(path_to_file)
 
 main("books/frankenstein.txt")
