11a463aa10171062c373f73d6557af5c8e28128c
jsearls-gh <jsearls90@gmail.com>
2024-11-18 16:47:35 -0500

    add main.py function to print character count dictionary

--- main.py
+++ main.py
@@ -9,8 +9,23 @@ def print_word_count(path_to_file):
         words = file_contents.split()
         print(len(words))
 
+def print_character_counts(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        lowered_contents = file_contents.lower()
+        char_counts = {}
+        for char in lowered_contents:
+            if char_counts.get(char) is None:
+                char_counts[char] = 1
+            else:
+                char_counts[char] += 1
+
+        print(char_counts)
+            
+
 def main(path_to_file):
     #print_contents(path_to_file)
-    print_word_count(path_to_file)
+    #print_word_count(path_to_file)
+    print_character_counts(path_to_file)
 
 main("books/frankenstein.txt")
