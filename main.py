1dc58632eaff8430f8e276cb413b21a4a6f02410
jsearls-gh <jsearls90@gmail.com>
2024-11-18 17:17:44 -0500

    update main.py to print report, accept book path from args

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+import sys
+
 def get_contents(file_path):
     with open(file_path) as f:
         return f.read()
-    
 
-def print_contents(file_contents):
-   print(file_contents)
-
-def print_word_count(file_contents):
+def get_word_count(file_contents):
     words = file_contents.split()
-    print(len(words))
+    return len(words)
 
-def print_character_counts(file_contents):
+def get_character_counts(file_contents):
     lowered_contents = file_contents.lower()
     char_counts = {}
     for char in lowered_contents:
@@ -19,14 +17,35 @@ def print_character_counts(file_contents):
         else:
             char_counts[char] += 1
 
-    print(char_counts)
+    return char_counts
+
+def sort_on_count(single_char_dict):
+    return single_char_dict["count"]
+
+def format_character_counts(char_count_dict):
+    char_count_list = []
+    for char in char_count_dict.keys():
+        if char.isalpha():
+            char_count_list.append({ "char": char, "count": char_count_dict[char] })
+    
+    char_count_list.sort(reverse = True, key = sort_on_count)
+
+    return char_count_list
             
 
 def main(path_to_file):
     file_contents = get_contents(path_to_file)
-    #print_contents(file_contents)
-    #print_word_count(file_contents)
-    print_character_counts(file_contents)
+
+    word_count = get_word_count(file_contents)
+    character_counts = format_character_counts(get_character_counts(file_contents))
+
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document\r\n")
+    for char_dict in character_counts:
+        print(f"The '{char_dict["char"]}' character was found {char_dict["char"]} times")
+    print("--- End report ---")
 
 if __name__ == '__main__':
-    main("books/frankenstein.txt")
+    file_path = sys.argv[1]
+    
+    main(file_path)
